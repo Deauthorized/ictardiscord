58cd6b5ad2fb514d237fe87d671b53aac1673483
Deauthorized <Deauthorized@protonmail.com>
2020-01-21 06:12:37 +0000

    Update restart.js

--- commands/restart.js
+++ commands/restart.js
@@ -10,7 +10,7 @@ module.exports = {
         else
         {
             await message.reply("Restarting.");
-            await client.user.setStatus(idle, "brb");
+            await client.user.setStatus('idle', 'brb');
             process.exit();
         }
 	},
