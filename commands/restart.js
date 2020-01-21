3c83481d12556a2cf551996258a8908378b0eb71
Deauthorized <Deauthorized@protonmail.com>
2020-01-21 06:07:37 +0000

    Update restart.js

--- commands/restart.js
+++ commands/restart.js
@@ -2,16 +2,15 @@ const sleep = require('system-sleep');
 module.exports = {
 	name: 'restart',
 	description: 'restarts the bot',
-	execute(message, args, client, botmaster) {
+	async execute(message, args, client, botmaster) {
         if (parseInt(message.author.id) !== botmaster)
         {
             message.reply("You can't do this.");
         }
         else
         {
-            message.reply("Restarting.");
-            client.user.setPresence({ game: { name: 'brb', type: "idle"}});
-            sleep(500)
+            await message.reply("Restarting.");
+            await client.user.setPresence({ game: { name: 'brb', type: "idle"}});
             process.exit();
         }
 	},
