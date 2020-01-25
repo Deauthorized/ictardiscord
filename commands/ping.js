f9776bc74b97ec7343e4aadc95ef078d6bb66ee2
Deauthorized <Deauthorized@protonmail.com>
2020-01-25 17:39:22 +0000

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -2,6 +2,6 @@ module.exports = {
 	name: 'ping',
 	description: 'check latency of discords websocket',
 	execute(message, args, client) {
-		message.channel.send("Pong! '" + str(client.ping).substring(0, 6) + "ms'");
+		message.channel.send("Pong! '" + client.ping.toString().substring(0, 6) + "ms'");
 	},
 };
