6e7dfd451c6971477b7c60c88a48e6c89987bfda
Deauthorized <Deauthorized@protonmail.com>
2020-01-25 19:37:05 +0000

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,14 +1,15 @@
+const Discord = require('discord.js');
 module.exports = {
 	name: 'help',
 	description: 'gets current cmds',
 	execute(message, args, client) {
-        const helpEmbed = new Discord.MessageEmbed()
+        const helpEmbed = new Discord.RichEmbed()
             .setColor('#0099ff')
             .setTitle(`${client.commands.size} commands`)
             .setAuthor('Ictar', client.user.avatar, 'https://discord.js.org')
         client.commands.forEach(element => {
             helpEmbed.addField(`=${element.name}`, `${element.description}`, true);
         });
-        message.channel.send({helpEmbed});
+        message.channel.send(helpEmbed);
 	},
 };
