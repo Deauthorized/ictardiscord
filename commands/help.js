f0b4d70662ea84f09b9b91157417b72ccf5e5d4e
Deauthorized <Deauthorized@protonmail.com>
2020-01-25 19:32:52 +0000

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -6,9 +6,10 @@ module.exports = {
         const helpEmbed = new Discord.MessageEmbed()
             .setColor('#0099ff')
             .setTitle(`${client.commands.size} commands`)
+            .setAuthor('Ictar', client.user.avatar, 'https://discord.js.org')
         client.commands.forEach(element => {
-            helpEmbed.addField(`${element.name}`, `${element.description}`)
+            helpEmbed.addField(`${prefix}${element.name}`, `${element.description}`, true);
         });
-        message.channel.send(helpEmbed);
+        message.channel.send({helpEmbed});
 	},
 };
