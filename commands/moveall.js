103641efc2101a81526a1e25ccadfb607fce4646
Deauthorized <Deauthorized@protonmail.com>
2020-01-25 16:22:14 +0000

    Create moveall.js

---
+++ commands/moveall.js
@@ -0,0 +1,30 @@
+module.exports = {
+	name: 'moveall',
+	description: 'moves everyone to a single voice channel',
+	async execute(message, args, client) {
+        if (!message.member.hasPermission('ADMINISTRATOR', false, false))
+        {
+            message.reply("You can't do this.");
+        }
+        else
+        {   
+            toChannel = client.channels.get(args[0]);
+            if (message.channel.guild !== toChannel.guild){
+                message.reply("Cross server commands are disallowed.");
+            }
+            else
+            {
+                message.reply(`Moving everyone to #${toChannel.name}`);
+                message.guild.channels.forEach(element =>
+                {
+                    if (element.type == "voice")
+                    {
+                        element.members.forEach(element => {
+                            element.setVoiceChannel(toChannel);
+                        });
+                    }
+                });
+            }
+        }
+	},
+};
