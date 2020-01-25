e68d4db0e3c07ec1395443bbce45a9e40adc6ed9
Deauthorized <Deauthorized@protonmail.com>
2020-01-25 19:28:07 +0000

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,11 +1,14 @@
+const { prefix } = require('./config.json');
 module.exports = {
 	name: 'help',
 	description: 'gets current cmds',
-	execute(message, args, client, prefix) {
-        help = `\`\`\`${client.commands.size} commands`
+	execute(message, args, client) {
+        const helpEmbed = new Discord.MessageEmbed()
+            .setColor('#0099ff')
+            .setTitle(`${client.commands.size} commands`)
         client.commands.forEach(element => {
-            help += `\n=${element.name} > ${element.description}`
+            helpEmbed.addField(`${element.name}`, `${element.description}`)
         });
-        message.channel.send(help + '```')
+        message.channel.send(helpEmbed);
 	},
 };
