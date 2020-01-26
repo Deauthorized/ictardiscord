5ebb9238ae910bfda58e43c48e5fac86256a1e52
Deauthorized <Deauthorized@protonmail.com>
2020-01-26 16:37:54 +0000

    Update 8ball.js

--- commands/8ball.js
+++ commands/8ball.js
@@ -1,7 +1,8 @@
 const sleep = require('system-sleep');
 module.exports = {
 	name: '8ball',
-	description: 'have a question that only a randomly generated answer can sate?',
+    description: 'have a question that only a randomly generated answer can sate?',
+    usage: '=8ball <question>',
 	execute(message, args, client) {
         var answers = ["sure why not", "go ahead", "yeah", "true", "idk", "probably", "not sure tbh", "uhhh", "naw", "dont think so", "gonna go with a no", "no"];
         var answer = answers[Math.floor(Math.random() * answers.length)];
