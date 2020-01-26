38c5a34fba9752a6a3e57f39985e32b0280d475c
Deauthorized <Deauthorized@protonmail.com>
2020-01-26 00:40:49 +0000

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -8,6 +8,10 @@ module.exports = {
         var idleCount = message.guild.members.filter(m => m.presence.status === 'idle').size
         var dndCount = message.guild.members.filter(m => m.presence.status === 'dnd').size
         var txtchnCount = message.guild.channels.filter(m => m.type === 'text').size
+        var voicechnCount = message.guild.channels.filter(m => m.type === 'voice').size
+        var newschnCount = message.guild.channels.filter(m => m.type === 'news').size
+        var strechnCount = message.guild.channels.filter(m => m.type === 'store').size
+        var activeChannels = message.guild.channels.filter(m => m.typing).name
         const infoEmbed = new Discord.RichEmbed()
             .setColor('#8527ce')
             .setTitle(`Server Info`)
@@ -21,6 +25,10 @@ module.exports = {
             .addField('Verified', `${message.guild.verified}`, true)
             .addField('Roles', `${message.guild.roles.size}`, true)
             .addField('Text Channels', `${txtchnCount}`, true)
+            .addField('Voice Channels', `${voicechnCount}`, true)
+            .addField('News Channels', `${newschnCount}`, true)
+            .addField('Store Channels', `${strechnCount}`, true)
+            .addField('Active Channels', `${activeChannels}`, true)
             .setFooter(`Server ID: ${message.guild.id} - DOB: ${message.guild.createdAt}`);
         message.channel.send(infoEmbed);
 	},
