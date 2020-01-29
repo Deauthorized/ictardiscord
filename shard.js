b33204c251acb6206e5fdf86bd163828900a655a
Deauthorized <Deauthorized@protonmail.com>
2020-01-29 01:56:54 +0000

    Update shard.js

--- shard.js
+++ shard.js
@@ -1,5 +1,5 @@
 const { ShardingManager } = require('discord.js');
-const manager = new ShardingManager('./Desktop/ictar/ictardiscord/Ictar.js', { token: process.env.TOKEN });
+const manager = new ShardingManager(__dirname + 'Ictar.js', { token: process.env.TOKEN });
 
 
 manager.spawn();
