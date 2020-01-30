2ea0ffc1d8e30fb5affff1d941ffe6803136e42d
Deauthorized <Deauthorized@protonmail.com>
2020-01-30 00:56:43 +0000

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -2,9 +2,18 @@ const Discord = require('discord.js');
 module.exports = {
 	name: 'userinfo',
     description: 'get info about a specific user (testing stage, probably wont work)',
-    usage: '=userinfo <userid or mention>',
+    usage: '=userinfo <mention>',
 	execute(message, args, client) {
+        if (message.mentions.users.first() === undefined)
+        {
+            return message.reply("Not a valid member.")
+        }
         let user = message.mentions.users.first()
-        message.reply(user.tag);
+        const infoEmbed = new Discord.RichEmbed()
+            .setColor('#8527ce')
+            .setTitle(`User Info`)
+            .setThumbnail(`${user.avatarURL}`)
+            .setAuthor(`${message.user.username}`, message.user.avatarURL, 'https://discord.js.org')
+        message.channel.send(infoEmbed)
 	},
 };
