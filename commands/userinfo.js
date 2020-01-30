faf671b67f04f9afe86b3ca2efc9212b885106d5
Deauthorized <Deauthorized@protonmail.com>
2020-01-30 01:04:07 +0000

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -9,11 +9,15 @@ module.exports = {
             return message.reply("Not a valid member.")
         }
         var usr = message.mentions.users.first()
+        var gldusr = message.guild.member(usr)
         const infoEmbed = new Discord.RichEmbed()
             .setColor('#8527ce')
             .setTitle(`User Info`)
             .setThumbnail(`${usr.avatarURL}`)
             .setAuthor(`${usr.username}`, usr.avatarURL, 'https://discord.js.org')
+            .addField('Tag', `<@${usr.id}>`)
+            .addField('Register Date', usr.createdAt)
+            .addField('Server Join Date', gldusr.joinedAt)
         message.channel.send(infoEmbed)
 	},
 };
