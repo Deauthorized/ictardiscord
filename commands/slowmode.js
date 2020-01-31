20775fb7c40f7d58c0e2c5530ef16a917bf0eb33
Deauthorized <Deauthorized@protonmail.com>
2020-01-31 23:20:17 +0000

    Update slowmode.js

--- commands/slowmode.js
+++ commands/slowmode.js
@@ -1,31 +1,25 @@
 module.exports = {
-	name: 'slowmode',
-    	description: 'sets the channels slowmode value',
-    	usage: '=slowmode <int|off>',
+    name: 'slowmode',
+    description: 'sets the channels slowmode value, use "off" to disable slowmode',
+    usage: '=slowmode <int|off>',
+    perms: 'MANAGE_CHANNELS',
 	execute(message, args, client) {
-        if (!message.member.hasPermission('ADMINISTRATOR', false, false))
-        {
-            message.reply("You don't have authority over this channel.");
-        }
-        else
-        {
-            if (args[0] == "off")
+        if (args[0] == "off")
             {
                 message.channel.setRateLimitPerUser(0);
-                message.reply("Disabled slowmode.")
+                message.reply("Slowmode has been disabled.")
                 return;
             }
             if (isNaN(args[0]) || parseInt(args[0]) > 21600 || parseInt(args[0]) < 1)
             {
-                message.reply(`Illegal argument. (${args[0]})`);
+                message.reply(`Illegal argument.`);
                 return;
             }
             else
             {   
                 message.channel.setRateLimitPerUser(args[0]);
-                message.reply("Successfully set slowmode to " + parseInt(args[0]))
+                message.reply(`Slowmode value has been set to ${parseInt(args[0])} seconds.`)
                 return;
             }
-        }
 	},
 };
