b9736625851b324b382168a7016798a7fbcf638c
Deauthorized <Deauthorized@protonmail.com>
2020-01-31 23:15:50 +0000

    Update moveall.js

--- commands/moveall.js
+++ commands/moveall.js
@@ -4,13 +4,7 @@ module.exports = {
     usage: '=moveall <channel_id>',
     perms: 'MOVE_MEMBERS',
 	async execute(message, args, client) {
-        if (!message.member.hasPermission('MOVE_MEMBERS', false, false))
-        {
-            message.reply("You can't do this.");
-        }
-        else
-        {   
-            toChannel = client.channels.get(args[0]);
+        toChannel = client.channels.get(args[0]);
             if (message.channel.guild !== toChannel.guild){
                 message.reply("Cross server commands are disallowed.");
             }
@@ -35,6 +29,5 @@ module.exports = {
                 });
                 await msage.edit(`<@${message.author.id}>, Moved ${movecount} user(s) to :speaker:${toChannel.name}`);
             }
-        }
-	},
+    }
 };
