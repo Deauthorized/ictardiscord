479664faca2572d820bcb54f08527ce09b3e7e16
Deauthorized <Deauthorized@protonmail.com>
2020-01-31 06:03:43 +0000

    Update serverinfo.js

--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -5,9 +5,9 @@ module.exports = {
     usage: '=serverinfo',
 	execute(message, args, client, botmaster, guildConf) {
         var onlineCount = message.guild.members.filter(m => m.presence.status === 'online').size
-        var offlineCount = message.guild.members.filter(m => m.presence.status === 'offline').size
         var idleCount = message.guild.members.filter(m => m.presence.status === 'idle').size
         var dndCount = message.guild.members.filter(m => m.presence.status === 'dnd').size
+        var offlineCount = message.guild.memberCount - onlineCount - idleCount - dndCount
         var txtchnCount = message.guild.channels.filter(m => m.type === 'text').size
         var voicechnCount = message.guild.channels.filter(m => m.type === 'voice').size
         var newschnCount = message.guild.channels.filter(m => m.type === 'news').size
@@ -27,7 +27,7 @@ module.exports = {
             .setColor('#8527ce')
             .setTitle(`Server Info`)
             .setThumbnail(`${message.guild.iconURL}`)
-            .addField('General Info', `:bust_in_silhouette: ${onlineCount + idleCount + dndCount} / ${message.guild.memberCount} members are currently logged in. \n<:online:670792653314588702> ${onlineCount} members are online \n<:idle:670792653251674122> ${idleCount} members are AFK \n<:dnd:670792653012729858> ${dndCount} members are DND \n <:offline:670792652861472840> ${offlineCount} members are offline `)
+            .addField('General Info', `:bust_in_silhouette: ${onlineCount + idleCount + dndCount} / ${message.guild.memberCount} members are currently logged in. \n<:online:670792653314588702> ${onlineCount} members are online \n<:idle:670792653251674122> ${idleCount} members are AFK \n<:dnd:670792653012729858> ${dndCount} members are DND \n <:offline:670792652861472840> ${offlineCount} members are offline or invisible `)
             .setAuthor(`${message.guild.name} / ${message.guild.nameAcronym}`, message.guild.iconURL, 'https://discord.js.org')
             .addField('Server Description', `${serverdesc}`)
             .addField('Members', `${message.guild.memberCount}`, true)
