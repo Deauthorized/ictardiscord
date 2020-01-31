3243f09d3e9013d2627bd6dcbbd564695b8d6bd5
Deauthorized <Deauthorized@protonmail.com>
2020-01-31 05:05:37 +0000

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -8,8 +8,16 @@ module.exports = {
         {
             return message.reply("Not a valid member.")
         }
-        var usr = message.mentions.users.first()
-        var gldusr = message.guild.member(usr)
+        else if (message.guild.member(args[0]))
+        {
+            var usr = client.fetchUser(args[0])
+            var gldusr = message.guild.member(usr)
+        }
+        else
+        {
+            var usr = message.mentions.users.first()
+            var gldusr = message.guild.member(usr)
+        }
         const infoEmbed = new Discord.RichEmbed()
             .setColor('#8527ce')
             .setTitle(`User Info`)
@@ -18,7 +26,7 @@ module.exports = {
             .addField('Tag', `<@${usr.id}>`, true)
             .addField('Register Date', usr.createdAt, true)
             .addField('Server Join Date', gldusr.joinedAt, true)
-            .addField(`Roles (${gldusr.roles.size})`, gldusr.roles.map(role => `<@&${role.id}>`))
+            .addField(`Roles (${gldusr.roles.size})`, gldusr.roles.map(role => `<@&${role.id}>`).join(" "))
         message.channel.send(infoEmbed)
 	},
 };
