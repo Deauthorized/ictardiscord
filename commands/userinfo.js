92cbb076a1a0932ff56d3b3ef4883b6ee6f45ab2
Deauthorized <Deauthorized@protonmail.com>
2020-01-31 05:30:16 +0000

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -6,21 +6,10 @@ module.exports = {
 	execute(message, args, client) {
         if (message.mentions.users.first() === undefined)
         {
-            if (message.guild.member(args[0]) & client.fetchUser(args[0]))
-            {
-                var usr = client.fetchUser(args[0])
-                var gldusr = message.guild.member(usr) 
-            }
-        }
-        else if (message.mentions.users.first() !== undefined)
-        {
-            var usr = message.mentions.users.first()
-            var gldusr = message.guild.member(usr)
-        }
-        else
-        {
             return message.reply("Not a valid user.");
         }
+        var usr = message.mentions.users.first()
+        var gldusr = message.guild.member(usr)
         const infoEmbed = new Discord.RichEmbed()
             .setColor('#8527ce')
             .setTitle(`User Info`)
@@ -30,6 +19,7 @@ module.exports = {
             .addField('Register Date', usr.createdAt, true)
             .addField('Server Join Date', gldusr.joinedAt, true)
             .addField(`Roles (${gldusr.roles.size})`, gldusr.roles.map(role => `<@&${role.id}>`).join(" "))
+            .addField(`Permissions ()`, gldusr.permissions.map(prm => ))
         message.channel.send(infoEmbed)
 	},
 };
