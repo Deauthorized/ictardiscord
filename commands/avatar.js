686a478c79f64b7569846b038cb5018352dc5ab2
Deauthorized <Deauthorized@protonmail.com>
2020-02-01 00:44:17 +0000

    Create avatar.js

---
+++ commands/avatar.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js');
+module.exports = {
+    name: 'avatar',
+    description: 'get a users avatar',
+    usage: '=avatar <mention>',
+	execute(message, args, client) {
+        if (message.mentions.users.first() === undefined)
+        {
+            var usr = message.author
+        }
+        else
+        {
+            var usr = message.mentions.users.first()
+        }
+        var gldusr = message.guild.member(usr)
+        const infoEmbed = new Discord.MessageEmbed()
+            .setColor('#8527ce')
+            .setTitle(`Avatar`)
+            .setAuthor(`${usr.username}`, usr.avatarURL(), 'https://discord.js.org')
+            .setImage(usr.avatarURL())
+        message.channel.send(infoEmbed)
+	},
+};
