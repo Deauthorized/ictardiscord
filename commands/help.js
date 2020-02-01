937f4c870c32f7a4dfdb18d08e6b2cc2af810f6a
Deauthorized <Deauthorized@protonmail.com>
2020-02-01 01:01:16 +0000

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -10,7 +10,7 @@ module.exports = {
             const helpEmbed = new Discord.MessageEmbed()
                 .setColor('#8527ce')
                 .setTitle(`${client.commands.size} commands`)
-                .setAuthor('Ictar', client.user.avatarURL, 'https://discord.js.org')
+                .setAuthor('Ictar', client.user.avatarURL(), 'https://discord.js.org')
             client.commands.forEach(element => {
                 help += `\n${guildConf.prefix}${element.name} - ${element.description}` 
             });
@@ -34,7 +34,7 @@ module.exports = {
                 if (help === "undefined"){help = "No usage info is specified yet"}
                 const helpEmbed = new Discord.MessageEmbed()
                     .setColor('#8527ce')
-                    .setAuthor('Ictar', client.user.avatarURL, 'https://discord.js.org')
+                    .setAuthor('Ictar', client.user.avatarURL(), 'https://discord.js.org')
                     .addField(`Usage for ${guildConf.prefix}${args[0]}`, `${help}`)
                 message.channel.send(helpEmbed);
             }
