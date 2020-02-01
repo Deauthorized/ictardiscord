23265cf078f0b1b3330722184868d424e0183c41
Deauthorized <Deauthorized@protonmail.com>
2020-02-01 04:42:31 +0000

    Update userinfo.js

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -4,30 +4,13 @@ module.exports = {
     description: 'get info about a specific user',
     usage: '=userinfo <mention>',
 	execute(message, args, client) {
-        function getUserFromMention(mention) {
-            if (!mention) return;
-        
-            if (mention.startsWith('<@') && mention.endsWith('>')) {
-                mention = mention.slice(2, -1);
-        
-                if (mention.startsWith('!')) {
-                    mention = mention.slice(1);
-                }
-        
-                return client.users.get(mention);
-            }
-            else
-            {
-                return client.users.get(mention);
-            }
-        }
         if (args[0] === undefined)
         {
             var usr = message.author
         }
         else
         {
-            var usr = getUserFromMention(args[0])
+            var usr = client.functions.get("parsemention").getUserFromMention(args[0], client)
         }
         var gldusr = message.guild.member(usr)
         if (usr === undefined)
