91428a324fd75d8877236ecc29f4ef0a499671df
Deauthorized <Deauthorized@protonmail.com>
2020-02-01 04:31:17 +0000

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -4,15 +4,36 @@ module.exports = {
     description: 'get a users avatar',
     usage: '=avatar <mention>',
 	execute(message, args, client) {
-        if (message.mentions.users.first() === undefined)
+        function getUserFromMention(mention) {
+            if (!mention) return;
+        
+            if (mention.startsWith('<@') && mention.endsWith('>')) {
+                mention = mention.slice(2, -1);
+        
+                if (mention.startsWith('!')) {
+                    mention = mention.slice(1);
+                }
+        
+                return client.users.get(mention);
+            }
+            else
+            {
+                return client.users.get(mention);
+            }
+        }
+        if (args[0] === undefined)
         {
             var usr = message.author
         }
         else
         {
-            var usr = message.mentions.users.first()
+            var usr = getUserFromMention(args[0])
         }
         var gldusr = message.guild.member(usr)
+        if (usr === undefined)
+        {
+            return message.reply("Could not find specified user.")
+        }
         const infoEmbed = new Discord.MessageEmbed()
             .setColor('#8527ce')
             .setTitle(`Avatar`)
