9b0acf439090f0e631dd305e53bec17d6b1eb3b1
Deauthorized <Deauthorized@protonmail.com>
2020-02-02 01:58:40 +0000

    Update stats.js

--- commands/stats.js
+++ commands/stats.js
@@ -1,4 +1,11 @@
 const Discord = require('discord.js');
+const pm2 = require("pm2");
+const promises = 
+[
+    client.shard.fetchClientValues('guilds.size'),
+    client.shard.fetchClientValues('users.size'),
+    client.shard.fetchClientValues('channels.size')
+]
 module.exports = {
     name: 'stats',
     description: 'bot stats',
@@ -9,22 +16,13 @@ module.exports = {
         .setTitle(`Ictar Stats`)
         .setThumbnail(`${client.user.avatarURL()}`)
         .setAuthor(`${client.user.tag}`, client.user.avatarURL(), 'https://discord.js.org')
-        await client.shard.fetchClientValues('guilds.size')
+        Promise.all(promises)
             .then(results =>
-            {
-                infoEmbed.addField('Servers', `${results.reduce((prev, guildCount) => prev + guildCount, 0)} servers`, true)
-            })
-        await client.shard.fetchClientValues('users.size')
-            .then(results =>
-            {
-                infoEmbed.addField('Users', `${results.reduce((prev, usrCount) => prev + usrCount, 0)} users`, true)
-            })
-
-        await client.shard.fetchClientValues('channels.size')
-            .then(results =>
-            {
-                infoEmbed.addField('Channels', `${results.reduce((prev, chnlCount) => prev + chnlCount, 0)} channels`, true)
-            })
+                {
+                    infoEmbed.addField('Servers', `${results[0].reduce((prev, guildCount) => prev + guildCount, 0)} servers`, true)
+                    infoEmbed.addField('Users', `${results[1].reduce((prev, usrCount) => prev + usrCount, 0)} users`, true)
+                    infoEmbed.addField('Channels', `${results[2].reduce((prev, chnlCount) => prev + chnlCount, 0)} channels`, true)
+                })
 
         message.channel.send(infoEmbed) 
 	},
