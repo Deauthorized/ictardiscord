aaad35ce10ebfbca6a6b24375535abe689aecdda
Deauthorized <Deauthorized@protonmail.com>
2020-02-02 19:04:01 +0000

    Delete ping.js

--- commands/ping.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	name: 'ping',
-	description: 'check latency of discords websocket',
-	execute(message, args, client) {
-		message.channel.send("Pong! Latency is about `" + client.ws.ping.toString().substring(0, 6) + "ms`");
-	},
-};
