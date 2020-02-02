a2fcb92766440f8a1f1bec217bf6c01ac3b20d12
Deauthorized <Deauthorized@protonmail.com>
2020-02-02 20:28:50 +0000

    Update returnhelpembed.js

--- functions/returnhelpembed.js
+++ functions/returnhelpembed.js
@@ -8,9 +8,10 @@ module.exports = {
                 .setColor('#8527ce')
                 .setTitle(`${client.commands.size} commands`)
                 .setAuthor('Ictar', client.user.avatarURL(), 'https://discord.js.org')
-                client.commands.forEach(element => {
-                    helpEmbed.addField(element.category, `\n${guildConf.prefix}${element.name} - ${element.description}`)
-                });
+                client.commands.forEach(element =>
+                {
+                    help += `\n${guildConf.prefix}${element.name} - ${element.description}` 
+            });
             return helpEmbed;
         }
 };
