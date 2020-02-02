26c2c6cce59bbc563b253727e0bee84d046e65af
Deauthorized <Deauthorized@protonmail.com>
2020-02-02 19:55:20 +0000

    Add files via upload

---
+++ functions/returnhelpembed.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+module.exports = {
+    name: 'helpembed',
+        get(client)
+        {
+            help = String();
+            const helpEmbed = new Discord.MessageEmbed()
+                .setColor('#8527ce')
+                .setTitle(`${client.commands.size} commands`)
+                .setAuthor('Ictar', client.user.avatarURL(), 'https://discord.js.org')
+                client.commands.forEach(element => {
+                    helpEmbed.addField(element.category, `\n${guildConf.prefix}${element.name} - ${element.description}`)
+                });
+            return helpEmbed
+        }
+};
