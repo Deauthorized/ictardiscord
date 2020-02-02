d8f75d0db86639dac6f37acea628c40ab97ae559
Deauthorized <Deauthorized@protonmail.com>
2020-02-02 00:16:05 +0000

    Update 8ball.js

--- commands/8ball.js
+++ commands/8ball.js
@@ -4,6 +4,7 @@ module.exports = {
     description: 'have a question that only a randomly generated answer can sate?',
     usage: '=8ball <question>',
 	execute(message, args, client) {
+        // probably gonna depreciate this soon as i don't see a use for it
         var answers = ["sure why not", "go ahead", "yeah", "true", "idk", "probably", "not sure tbh", "uhhh", "naw", "dont think so", "gonna go with a no", "no"];
         var answer = answers[Math.floor(Math.random() * answers.length)];
         message.reply(content=":8ball: A reply has been recieved"
