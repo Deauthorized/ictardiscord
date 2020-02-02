2a5f8adf0091e6d0f9d9e4de442d4f79852aba09
Deauthorized <Deauthorized@protonmail.com>
2020-02-02 20:24:20 +0000

    Update Ictar.js

--- Ictar.js
+++ Ictar.js
@@ -19,7 +19,7 @@ for (const folder of commandDir) {
         {
             const command = require(__dirname + `/commands/${folder}/${cmd}`)
             client.commands.set(command.name, command);
-            client.categories.set(command, folder);
+            client.categories.set(folder, command);
         }
     }
     catch (error)
