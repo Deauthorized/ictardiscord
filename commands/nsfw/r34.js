5e2b8cb38863a6d0817dff1d9ab8de4deae58b59
Deauthorized <Deauthorized@protonmail.com>
2020-02-05 23:52:09 +0000

    Create r34.js

---
+++ commands/nsfw/r34.js
@@ -0,0 +1,10 @@
+module.exports = {
+	name: 'r34',
+    description: 'retrieve a post from rule34, do **NOT** search for anything that violates the TOS',
+    usage: '=nsfw <query>',
+    category: 'nsfw',
+    nsfw: true,
+	execute(message, args, client) {
+        message.reply("Command is in development and is not functional (yet)")
+	},
+};
