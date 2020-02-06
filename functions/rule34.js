a44a75df7decdaf8f1d3986e02f6c96f1fbc9d5d
Deauthorized <Deauthorized@protonmail.com>
2020-02-06 02:42:04 +0000

    Update rule34.js

--- functions/rule34.js
+++ functions/rule34.js
@@ -14,9 +14,23 @@ module.exports = {
     name: 'rule34',
         get(query)
         {
+            query = query.replace(" ", "+")
             rq.get(`https://r34-json-api.herokuapp.com/posts?limit=1&tags=${query}`, {json: true}, (e,r,body) =>
             {
-                return body[0];
+                if (e) { return console.log(e); }
+                b = body[0]
+                if (body === "[]")
+                {
+                    return "API_DOWN";
+                }
+                for (i of blacklistedtags)
+                {
+                    if (body[0].tags.includes(i))
+                    {
+                        return "BLACKLISTED_TAG";
+                    }
+                }
+                return b;
             })
         }
 };
