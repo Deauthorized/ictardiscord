f564f56dd61e9362ab00132a54d1cc02aa9b7c8d
Deauthorized <Deauthorized@protonmail.com>
2020-02-06 00:11:29 +0000

    Update rule34.js

--- functions/rule34.js
+++ functions/rule34.js
@@ -1,9 +1,21 @@
 const rq = require('request');
+const blacklistedtags = 
+[
+    "cub",
+    "loli",
+    "shota",
+    "guro",
+    "gore"
+]
 module.exports = {
     name: 'rule34',
         get(query)
         {
             query = query.replace(" ", "+")
-            return query;
+            rq.get(`https://r34-json-api.herokuapp.com/posts?limit=10&tags=${query}`, {json:true}, function(e,r,b)
+            {
+                return r;
+            })
+            
         }
 };
