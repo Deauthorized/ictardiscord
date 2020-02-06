dc9729cc1e80df0c88750ca336022d74ebc07ff6
Deauthorized <Deauthorized@protonmail.com>
2020-02-06 00:17:26 +0000

    Update rule34.js

--- functions/rule34.js
+++ functions/rule34.js
@@ -1,21 +1,23 @@
-const rq = require('request');
+const { promisify } = require('util');
+const rq = promisify(require('request'));
 const blacklistedtags = 
 [
     "cub",
     "loli",
     "shota",
     "guro",
-    "gore"
+    "gore",
+    "underage"
 ]
 module.exports = {
     name: 'rule34',
         get(query)
         {
             query = query.replace(" ", "+")
-            rq.get(`https://r34-json-api.herokuapp.com/posts?limit=10&tags=${query}`, {json:true}, function(e,r,b)
-            {
-                return r;
-            })
-            
+            rq.get(`https://r34-json-api.herokuapp.com/posts?limit=1&tags=${query}`, {json:true})
+                .then(function(e,r,b)
+                {
+                    return r.statusCode
+                })
         }
 };
