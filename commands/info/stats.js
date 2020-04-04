f9004b5df47757a671fd7e3bcc28ec94f3687a8e
Deauthorized <Deauthorized@protonmail.com>
2020-04-04 17:14:28 -0400

    Update stats.js

--- commands/info/stats.js
+++ commands/info/stats.js
@@ -10,18 +10,18 @@ module.exports = {
         .setTitle(`Ictar Stats`)
         .setThumbnail(`${client.user.avatarURL()}`)
         .setAuthor(`${client.user.tag}`, client.user.avatarURL(), 'https://discord.js.org')
-        await client.shard.fetchClientValues('guilds.size')
+        await client.shard.fetchClientValues('guilds.cache.size')
             .then(results =>
             {
                 infoEmbed.addField('Servers', `${results.reduce((prev, guildCount) => prev + guildCount, 0)} servers`, true)
             })
-        await client.shard.fetchClientValues('users.size')
+        await client.shard.fetchClientValues('users.cache.size')
             .then(results =>
             {
                 infoEmbed.addField('Users', `${results.reduce((prev, usrCount) => prev + usrCount, 0)} users`, true)
             })
 
-        await client.shard.fetchClientValues('channels.size')
+        await client.shard.fetchClientValues('channels.cache.size')
             .then(results =>
             {
                 infoEmbed.addField('Channels', `${results.reduce((prev, chnlCount) => prev + chnlCount, 0)} channels`, true)
